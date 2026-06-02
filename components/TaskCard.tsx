9edf07585f9dc19ec084cc5944025cb1e68df2e2
12otherWays <reeturaj542@gmail.com>
2026-06-02 11:05:56 +0530

    refactor: abstract BarChart implementation into reusable TaskChart component

--- components/TaskCard.tsx
+++ components/TaskCard.tsx
@@ -59,43 +59,7 @@ export function TaskCard({ task, onToggleStatus }: TaskCardProps) {
       </div>
 
       {mounted && task.chartData && task.chartData.length > 0 && (
-        <div className="mt-4 h-48 w-full">
-          <ResponsiveContainer width="100%" height="100%">
-            <BarChart data={task.chartData}>
-              <XAxis
-                dataKey="name"
-                fontSize={12}
-                tickLine={false}
-                axisLine={false}
-                stroke="#888888"
-              />
-              <YAxis
-                fontSize={12}
-                tickLine={false}
-                axisLine={false}
-                stroke="#888888"
-                tickFormatter={(value) => `${value}`}
-              />
-              <Tooltip
-                contentStyle={{
-                  backgroundColor: "rgba(0,0,0,0.8)",
-                  border: "none",
-                  borderRadius: "8px",
-                  color: "#fff",
-                }}
-                itemStyle={{ color: "#fff" }}
-              />
-              <Bar dataKey="value" radius={[4, 4, 0, 0]}>
-                {task.chartData.map((entry, index) => (
-                  <Cell
-                    key={`cell-${index}`}
-                    fill={COLORS[index % COLORS.length]}
-                  />
-                ))}
-              </Bar>
-            </BarChart>
-          </ResponsiveContainer>
-        </div>
+        <TaskChart data={task.chartData} />
       )}
     </div>
   );
